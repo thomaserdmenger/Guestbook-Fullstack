81f178d2bb9a0699d919f9db5ecb098fd9bb7559
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-04-18 11:21:14 +0200

    Add Heading to Form Component and update styles

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -11,45 +11,48 @@ const Form = () => {
     console.log({ firstname, lastname, email, message })
   }
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center gap-2">
-      <input
-        className="border-[1.5px] w-6/12 rounded p-1"
-        type="text"
-        name="firstname"
-        placeholder="First Name"
-        value={firstname}
-        onChange={(e) => setFirstname(e.target.value)}
-      />
-      <input
-        className="border-[1.5px] w-6/12 rounded p-1"
-        type="text"
-        name="lastname"
-        placeholder="Last Name"
-        value={lastname}
-        onChange={(e) => setLastname(e.target.value)}
-      />
-      <input
-        className="border-[1.5px] w-6/12 rounded p-1"
-        type="email"
-        name="email"
-        placeholder="E-Mail"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        className="border-[1.5px] w-6/12 rounded p-1 h-20 mb-2"
-        type="text"
-        name="message"
-        placeholder="Leave a message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button
-        className="border-[1.5px] w-6/12 p-2 rounded-full border-none bg-[#B2E6D1] text-white font-bold"
-        type="submit">
-        Add message
-      </button>
-    </form>
+    <div className="flex flex-col">
+      <h1 className="text-center mt-8 mb-10 guestbook__heading text-5xl">Before You Go</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center gap-2">
+        <input
+          className="border-[1.5px] w-6/12 rounded p-1"
+          type="text"
+          name="firstname"
+          placeholder="First Name"
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+        <input
+          className="border-[1.5px] w-6/12 rounded p-1"
+          type="text"
+          name="lastname"
+          placeholder="Last Name"
+          value={lastname}
+          onChange={(e) => setLastname(e.target.value)}
+        />
+        <input
+          className="border-[1.5px] w-6/12 rounded p-1"
+          type="email"
+          name="email"
+          placeholder="E-Mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="border-[1.5px] w-6/12 rounded p-1 h-20 mb-2"
+          type="text"
+          name="message"
+          placeholder="Leave a message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          className="border-[1.5px] w-6/12 p-2 rounded-full border-none bg-[#B2E6D1] text-white font-bold"
+          type="submit">
+          Add message
+        </button>
+      </form>
+    </div>
   )
 }
 
