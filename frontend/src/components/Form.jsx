504cb92f7314d9dd9cfbc881b25e00497fc9a48e
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-04-18 11:16:13 +0200

    Add Form Component

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+
+const Form = () => {
+  const [firstname, setFirstname] = useState("")
+  const [lastname, setLastname] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log({ firstname, lastname, email, message })
+  }
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col items-center gap-2">
+      <input
+        className="border-[1.5px] w-6/12 rounded p-1"
+        type="text"
+        name="firstname"
+        placeholder="First Name"
+        value={firstname}
+        onChange={(e) => setFirstname(e.target.value)}
+      />
+      <input
+        className="border-[1.5px] w-6/12 rounded p-1"
+        type="text"
+        name="lastname"
+        placeholder="Last Name"
+        value={lastname}
+        onChange={(e) => setLastname(e.target.value)}
+      />
+      <input
+        className="border-[1.5px] w-6/12 rounded p-1"
+        type="email"
+        name="email"
+        placeholder="E-Mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="border-[1.5px] w-6/12 rounded p-1 h-20 mb-2"
+        type="text"
+        name="message"
+        placeholder="Leave a message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button
+        className="border-[1.5px] w-6/12 p-2 rounded-full border-none bg-[#B2E6D1] text-white font-bold"
+        type="submit">
+        Add message
+      </button>
+    </form>
+  )
+}
+
+export default Form
--- frontend/src/pages/Guestbook.jsx
+++ frontend/src/pages/Guestbook.jsx
@@ -1,10 +1,13 @@
+import Form from "../components/Form"
 import Header from "../components/Header"
 
 const Guestbook = () => {
   return (
     <>
       <Header />
-      <div>Guestbook</div>
+      <main>
+        <Form />
+      </main>
     </>
   )
 }
